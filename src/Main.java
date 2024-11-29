95e19bf3dacaa6ece16c84b2cc75b870c22cef54
Failemon <leminh10456@gmail.com>
2024-11-29 14:50:18 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main (String[] args){
-        System.out.println("Welcome to java, man");
+        System.out.println("Hello cu.\n im trying java");
     }
 }
